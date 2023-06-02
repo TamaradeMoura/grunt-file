59ffdb62e6e035f71368c9aff8beab3121c41a39
TamaradeMoura <tamara_moura_13@hotmail.com>
2023-06-01 22:19:13 -0300

    primeiro comit

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function(){
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const novaTarefa = $('#nome-tarefa').val();
+        const novoItem = $(`<li>${novaTarefa}</li>`);
+        (novoItem).appendTo('ol');
+        $(novoItem).fadeIn();
+        $('#nome-tarefa').val('');
+        
+        $('li').click(function(){
+            $(this).addClass('item-completado');
+        });
+    });
+});
+})
